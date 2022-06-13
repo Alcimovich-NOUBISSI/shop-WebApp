80adace5007ce657138d71a3308986e50eafe6b0
Alcimovich NOUBISSI <80033137+Alcimovich-NOUBISSI@users.noreply.github.com>
2022-06-13 19:24:00 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT= process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/products', productRoutes);
-aoi.use(express.static(path.join(__dirname + "/public")))
+api.use(express.static(path.join(__dirname + "/public")))
 
 app.listen(PORT, ()=>{
     console.log(`server up and running on port ${PORT}`)
