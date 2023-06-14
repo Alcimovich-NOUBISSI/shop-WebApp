9e6efae43140ddcfdb5ae3f197f591e88589c73e
Alcimovich-NOUBISSI <alcimovichnoubissi@gmail.com>
2023-06-14 05:09:33 +0100

    vercel

--- backend/server.js
+++ backend/server.js
@@ -42,3 +42,8 @@ if(process.env.ENVIRONMENT === "production") {
         res.send('Api running')
     })
 }
+
+app.use(express.static(path.join(__dirname, '/public')))
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, '/public', 'index.html' ))
+    })
